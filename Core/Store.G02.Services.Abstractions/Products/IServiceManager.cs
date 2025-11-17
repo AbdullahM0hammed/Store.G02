33c9caaa5b1b99c03fab3ce0dfb19e75dc83a062
Abdullah Emam <abdullah.mohammed.mahmoud.emam@gmail.com>
2025-11-17 16:23:48 +0200

    Add project files.

---
+++ Core/Store.G02.Services.Abstractions/Products/IServiceManager.cs
@@ -0,0 +1,21 @@
+﻿using Store.G02.Services.Abstractions.Orders;
+using Store.G02.Services.Abstractions.Products;
+using Store.G02.Services.Payments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G02.Services.Abstractions
+{
+    public interface IServiceManager
+    {
+        IProductService ProductService { get; }
+        IBasketService BasketService { get; }
+        ICacheService CacheService { get; }
+        IAuthService AuthService { get; }
+        IOrderService OrderService { get; }
+        IPaymentService PaymentService { get; }
+    }
+}
